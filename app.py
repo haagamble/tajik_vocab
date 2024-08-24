8338c47793286e7521461e3a6076f3ca1f93b0b5
haagamble <haagamble@yahoo.com>
2024-08-24 23:55:30 +0500

    added flash messages for user

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import random
 
 app = Flask(__name__)
 # app.secret_key = 'your_secret_key'
-app.secret_key = 'sixth_key'
+app.secret_key = 'key_24_key'
 
 # Load vocab.json
 with open('vocab.json', 'r', encoding='utf-8') as f:
@@ -33,12 +33,9 @@ def index():
         session['streak'] = 0
     if 'level' not in session:
         session['level'] = 1
+    if 'completed' not in session:
+        session['completed'] = False
 
-    # Check if the streak is a multiple of 3 and increase the level
-    # This is repeated in check-answer route
-    # if session['streak'] > 0 and session['streak'] % 3 == 0:
-    #     session['level'] += 1
-    
     # Get a new question for the current level
     word, choices, correct_answer = get_new_question(session['level'])
     
@@ -49,31 +46,41 @@ def index():
 def check_answer():
     user_answer = request.form['answer']
     correct_answer = request.form['correct_answer']
+
+    if 'completed' not in session:
+        session['completed'] = False
     
     # Check if the user's answer is correct
     if user_answer == correct_answer:
         session['streak'] += 1
         session['score'] += 1
         flash('Correct!', 'success')
+
+        # Check if the streak is a multiple of 3 and increase the level unless it is already 20
+        if session['streak'] > 0 and session['streak'] % 3 == 0 and session['level'] < 20:
+            session['level'] += 1
+            flash('You moved to the next level!', 'success') 
+        # Check if completed is true
+        elif session['completed'] == True:
+            flash('Keep going to increase your streak!', 'success')   
+        # Check if the streak is a multiple of 3 and the level is already 20
+        elif session['streak'] > 0 and session['streak'] % 3 == 0 and session['level'] == 20:
+            flash('Congratulations! You have completed all levels!', 'success')
+            # create variable completed is true
+            session['completed'] = True
         
-        # Check if level is 20 and streak is more than 31
-        if session['level'] == 20 and session['streak'] > 30:
-            flash('Keep going to get a longer streak!', 'success')
+        # else encourage the user to keep going
         else:
-            # Move to the next level if the streak is a multiple of 3
-            if session['streak'] % 3 == 0:
-                if session['level'] == 20 and session['streak'] <= 30:
-                    flash('Congratulations! You have completed all levels!', 'success')
-                else:
-                    flash('You moved to the next level!', 'success')
-                    session['level'] += 1
-            
+            flash('You are doing great!', 'success')
+     
     else:
         # set the streak to 0 if the answer is incorrect
-        # decrease level by 1 if the streak is 0
+        # decrease level by 1 unless level is at 1
+        # set completed to false
         session['streak'] = 0
         if session['level'] > 1:
             session['level'] -= 1
+        session['completed'] = False
         flash('Incorrect.', 'danger')
         
     
